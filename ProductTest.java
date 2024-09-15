09d9d2f89a2371bc0cd0b97edf6c33fcf0dede5d
simkhasa <160679001+simkhasa@users.noreply.github.com>
2024-09-15 18:46:18 -0400

    ProductTest.java

--- ProductTest.java
+++ ProductTest.java
@@ -5,44 +5,36 @@ class ProductTest {
     private Product product1;
     @BeforeEach
     void setUp() {
-        product1 = new Product("111","Muffin","Blueberry", 2.99);
+        product1 = new Product("111","cake","pink", 2.99);
     }
-
-    @Test
+     @Test
     void setName() {
-        product1.setName("Cupcake");
-        assertEquals("Cupcake", product1.getName());
+        product1.setName("muffin");
+        assertEquals("muffin", product1.getName());
     }
-
     @Test
     void setDescription() {
-        product1.setDescription("Chocolate");
-        assertEquals("Chocolate", product1.getDescription());
+        product1.setDescription("brownie");
+        assertEquals("brownie", product1.getDescription());
     }
-
     @Test
     void setPrice() {
         product1.setCost(3.99);
         assertEquals(3.99, product1.getCost());
     }
-
     @Test
     void testToString() {
-        assertEquals("Product{id='111', name='Muffin', description='Blueberry', price=2.99}", product1.toString());
+        assertEquals("Product{id='111', name='cake', description='pink', price=2.99}", product1.toString());
     }
-
     @Test
     void toCSV() {
-        assertEquals("111,Muffin,Blueberry,2.99", product1.toCSV());
+        assertEquals("111,cake,pink,2.99", product1.toCSV());
     }
-
     @Test
     void toJSON() {
-        assertEquals("{\"id\":\"111\", \"name\":\"Muffin\", \"description\":\"Blueberry\", \"price\":2.99}", product1.toJSON());
+        assertEquals("{\"id\":\"111\", \"name\":\"cake\", \"description\":\"pink\", \"price\":2.99}", product1.toJSON());
     }
-
     @Test
     void toXML() {
-        assertEquals("<Product><id>111</id><name>Muffin</name><description>Blueberry</description><price>2.99</price></Product>", product1.toXML());
-    }
-}
+        assertEquals("<Product><id>111</id><name>cake</name><description>pink</description><price>2.99</price></Product>", product1.toXML());
+    }}
