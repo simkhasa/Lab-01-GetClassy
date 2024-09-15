b42b02e7dcd4720f7f93cbbe7d70943d32867ce3
simkhasa <160679001+simkhasa@users.noreply.github.com>
2024-09-15 18:48:41 -0400

    SafeInputObject.java

--- SafeInputObject.java
+++ SafeInputObject.java
@@ -5,19 +5,17 @@ public class SafeInputObject {
     private Product product1;
     @BeforeEach
     void setUp() {
-        product1 = new Product("111", "Muffin", "Blueberry", 2.99);
+        product1 = new Product("111", "cake", "pink", 2.99);
     }
-
     @Test
     void setName() {
-        product1.setName("Cupcake");
-        assertEquals("Cupcake", product1.getName());
+        product1.setName("muffin");
+        assertEquals("muffin", product1.getName());
     }
-
     @Test
     void setDescription() {
-        product1.setDescription("Chocolate");
-        assertEquals("Chocolate", product1.getDescription());
+        product1.setDescription("brownie");
+        assertEquals("brownie", product1.getDescription());
     }
 
     @Test
@@ -28,21 +26,21 @@ public class SafeInputObject {
 
     @Test
     void testToString() {
-        assertEquals("Product{id='111', name='Muffin', description='Blueberry', price=2.99}", product1.toString());
+        assertEquals("Product{id='111', name='cake', description='pink', price=2.99}", product1.toString());
     }
 
     @Test
     void toCSV() {
-        assertEquals("111,Muffin,Blueberry,2.99", product1.toCSV());
+        assertEquals("111,cake,pink,2.99", product1.toCSV());
     }
 
     @Test
     void toJSON() {
-        assertEquals("{\"id\":\"111\", \"name\":\"Muffin\", \"description\":\"Blueberry\", \"price\":2.99}", product1.toJSON());
+        assertEquals("{\"id\":\"111\", \"name\":\"cake\", \"description\":\"pink\", \"price\":2.99}", product1.toJSON());
     }
 
     @Test
     void toXML() {
-        assertEquals("<Product><id>111</id><name>Muffin</name><description>Blueberry</description><price>2.99</price></Product>", product1.toXML());
+        assertEquals("<Product><id>111</id><name>cake</name><description>pink</description><price>2.99</price></Product>", product1.toXML());
     }
 }
